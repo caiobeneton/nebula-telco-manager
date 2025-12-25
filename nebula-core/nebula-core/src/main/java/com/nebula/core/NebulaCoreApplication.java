91ce6108d3727208f181fc7fbfcff9f332271127
Caio Beneton <caio.beneton@gmail.com>
2025-12-25 15:47:30 -0300

    Initial commit: Project Nebula Structure

---
+++ nebula-core/nebula-core/src/main/java/com/nebula/core/NebulaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulaCoreApplication.class, args);
+	}
+
+}
